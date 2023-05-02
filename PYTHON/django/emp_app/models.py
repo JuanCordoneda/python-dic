e5ec818b7b80fbfd06d98ea6109ca609125f9f0c
Juan Cruz Cordoneda <71473532+JuanCordoneda@users.noreply.github.com>
2023-05-02 10:02:36 -0300

    models

--- PYTHON/django/emp_app/models.py
+++ PYTHON/django/emp_app/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Lista de diferentes tipos de models.CharField en Django:
+# 1. CharField: Almacena texto corto con una longitud máxima determinada por max_length.
+# 2. TextField: Almacena texto largo con una longitud potencialmente ilimitada.
+# 3. SlugField: Almacena identificadores de URL legibles por humanos.
+# 4. EmailField: Almacena direcciones de correo electrónico válidas.
+# 5. URLField: Almacena direcciones URL válidas.
+# 6. BooleanField: Almacena valores booleanos (True o False).
+# 7. DateField: Almacena fechas.
+# 8. DateTimeField: Almacena fechas y horas.
+# 9. TimeField: Almacena horas.
+# 10. IntegerField: Almacena números enteros.
+# 11. PositiveIntegerField: Almacena números enteros positivos.
+# 12. PositiveSmallIntegerField: Almacena números enteros positivos más pequeños que un entero normal.
+# 13. SmallIntegerField: Almacena números enteros más pequeños que un entero normal.
+
+# ACA SE CREAN TODOS LOS MODELOS
 class Department(models.Model):
     name = models.CharField(max_length=100, null=False)
     location = models.CharField(max_length=100)
